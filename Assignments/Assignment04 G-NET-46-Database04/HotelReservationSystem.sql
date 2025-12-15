727fb5464ada32cef986dee1eeed05f0cec5567f
Youssef Hagr <166296630+youssefhagar@users.noreply.github.com>
2025-12-15 22:43:47 +0200

    Add files via upload

---
+++ Assignments/Assignment04 G-NET-46-Database04/HotelReservationSystem.sql
@@ -0,0 +1,93 @@
+﻿
+-- ======================
+-- 1. INSERT OPERATIONS : 
+-- ======================
+
+-- Insert a Guest (FullName, Nationality, PassportNumber, DateOfBirth) 
+INSERT INTO Guests (FullName, Nationality, ID_Passport_Number, DateOfBirth)
+VALUES ('Mohamed Ali', 'Egyptian', 'A12345678', '1995-06-15');
+
+--  Insert multiple Guests in one statement 
+INSERT INTO Guests (FullName, Nationality, ID_Passport_Number, DateOfBirth)
+VALUES 
+('Sara Ahmed', 'Egyptian', 'B98765432', '1998-02-20'),
+('John Smith', 'British', 'UK445566', '1990-11-10'),
+('Lina Hassan', 'Jordanian', 'J778899', '1996-08-05');
+
+
+-- ======================
+-- 2. UPDATE OPERATIONS 
+-- ======================
+
+-- Increase DailyRate by 15% for all suites 
+UPDATE Rooms
+SET DailyRate = DailyRate * 1.15
+WHERE RoomType = 'Suite';
+
+/* Update ReservationStatus: If CheckoutDate < GETDATE() → 
+'Completed' If CheckinDate > GETDATE() → 'Upcoming' Else → 
+'Active' */
+UPDATE Reservations
+SET Status =
+    CASE
+        WHEN CheckOutDate < GETDATE() THEN 'Completed'
+        WHEN CheckInDate > GETDATE() THEN 'Upcoming'
+        ELSE 'Active'
+    END;
+
+-- ======================
+-- 3. DELETE OPERATIONS 
+-- ======================
+
+-- Delete Reservation_Guest for a reservation 
+DELETE FROM Guest_Reservations
+WHERE ReservationID = 10;
+
+-- ======================
+-- 4. MERGE OPERATION 
+-- ======================
+
+/* Create table #StaffUpdates (StaffId, FullName, Position, Salary) 
+MERGE logic: 
+Match → Update Position + Salary 
+Not matched in Hotel DB → Insert 
+Not matched in Update table → Delete  */
+
+CREATE TABLE #StaffUpdates (
+    StaffID INT,
+    FullName VARCHAR(100),
+    Position VARCHAR(50),
+    Salary DECIMAL(10,2)
+);
+
+INSERT INTO #StaffUpdates
+VALUES
+(1, 'Ahmed Samy', 'Manager', 15000),
+(3, 'Nour Hassan', 'Receptionist', 6000),
+(20, 'Omar Adel', 'Cleaner', 4000);
+
+MERGE Staff AS Target
+USING #StaffUpdates AS Source
+ON Target.StaffID = Source.StaffID
+
+WHEN MATCHED THEN
+    UPDATE SET
+        Target.Position = Source.Position,
+        Target.Salary = Source.Salary
+
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (FullName, Position, Salary, HotelID)
+    VALUES (Source.FullName, Source.Position, Source.Salary, 1)
+
+WHEN NOT MATCHED BY SOURCE THEN
+    DELETE;
+
+
+-- Adding Some Data in hotel table for doing last Query (Merge)
+SELECT * FROM Hotels;
+
+INSERT INTO Hotels (HotelName, StarRating, Address, City, ContactNumber)
+VALUES
+('Royal Palace Hotel', 5, 'Nile Street', 'Cairo', '0223456789'),
+('Sea View Resort', 4, 'Beach Road', 'Alexandria', '0345678912'),
+('Golden Tower Hotel', 5, 'Downtown', 'Giza', '0234567891');
---
+++ Assignments/Assignment04 G-NET-46-Database04/OnlineRetailStore.sql
@@ -0,0 +1,164 @@
+﻿-- ================================
+-- 1. INSERT OPERATIONS : 
+-- ================================
+
+
+-- Insert a new Customer (FullName, PhoneNumber, Email, ShippingAddress, RegistrationDate) 
+INSERT INTO Customers (FullName, PhoneNumber, Email, ShippingAddress, RegistrationDate)
+VALUES ('Ahmed Mohamed', '01012345678', 'ahmed@gmail.com', 'Cairo, Egypt', GETDATE());
+
+-- Insert 3 new Suppliers 
+INSERT INTO Suppliers (SupplierName, ContactNumber, Email, Address, Country)
+VALUES 
+('Tech Supplier', '01211111111', 'tech@supplier.com', 'Nasr City', 'Egypt'),
+('Fashion Supplier', '01222222222', 'fashion@supplier.com', 'Giza', 'Egypt'),
+('Home Supplier', '01233333333', 'home@supplier.com', 'Alexandria', 'Egypt');
+
+
+-- Insert 2 Categories 
+INSERT INTO Categories (CategoryName, Description)
+VALUES 
+('Electronics', 'Electronic Devices'),
+('Clothing', 'Men and Women Clothes');
+
+-- Insert a Product but only (Name, UnitPrice) 
+INSERT INTO Products (ProductName, UnitPrice, StockQuantity, CategoryID)
+VALUES ('USB Flash', 80, 0, 1);
+
+-- Create table ArchivedStock (TranId, ProductId, QuantityChange,TranDate) Insert into ArchivedStock all StockTransactions before 2023 
+CREATE TABLE ArchivedStock (
+    TranId INT,
+    ProductID INT,
+    QuantityChange INT,
+    TranDate DATE
+);
+
+INSERT INTO ArchivedStock
+SELECT TransactionID, ProductID, QuantityChange, TransactionDate
+FROM StockTransactions
+WHERE TransactionDate < '2023-01-01';
+
+
+
+-- ================================
+-- 2. TEMPORARY TABLES 
+-- ================================
+
+-- Create #CustomerOrders with (OrderId, CustomerId, TotalAmount) Insert customers who made orders above 5000. 
+CREATE TABLE #CustomerOrders (
+    OrderID INT,
+    CustomerID INT,
+    TotalAmount DECIMAL(12,2)
+);
+
+INSERT INTO #CustomerOrders
+SELECT OrderID, CustomerID, TotalAmount
+FROM Orders
+WHERE TotalAmount > 5000;
+
+
+-- Create ##TopRatedProducts with (ProductId, Rating) Insert products with rating ≥ 4.5
+CREATE TABLE ##TopRatedProducts (
+    ProductID INT,
+    Rating DECIMAL(3,2)
+);
+
+INSERT INTO ##TopRatedProducts
+SELECT ProductID, AVG(Rating)
+FROM Reviews
+GROUP BY ProductID
+HAVING AVG(Rating) >= 4.5;
+
+
+-- ================================
+-- 3. UPDATE OPERATIONS 
+-- ================================
+
+-- Increase all UnitPrice by 10% for products under 100 EGP 
+UPDATE Products
+SET UnitPrice = UnitPrice * 1.10
+WHERE UnitPrice < 100;
+
+
+-- Update Order Status: If TotalAmount > 5000 → “Premium” Else → “Standard” 
+
+UPDATE Orders
+SET Status = 
+    CASE 
+        WHEN TotalAmount > 5000 THEN 'Premium'
+        ELSE 'Standard'
+    END;
+
+
+-- ================================
+-- 4. DELETE OPERATIONS 
+-- ================================
+
+-- Delete a Review by ReviewId 
+DELETE FROM Reviews
+WHERE ReviewID = 5;
+
+-- Delete all Orders with Status = “Cancelled 
+DELETE FROM Orders
+WHERE Status = 'Canceled';
+
+-- Delete OrderItems for a given OrderId 
+DELETE FROM OrderItems
+WHERE OrderID = 10;
+
+
+
+
+-- ================================
+-- 5. MERGE OPERATION 
+-- ================================
+/*
+Create table #ProductsUpdate (ProductId, Name, UnitPrice, 
+StockQuantity) 
+MERGE logic: 
+If product exists → UPDATE price & stock 
+If new → INSERT 
+DELETE 
+*/
+
+CREATE TABLE #ProductsUpdate (
+    ProductID INT,
+    Name VARCHAR(150),
+    UnitPrice DECIMAL(10,2),
+    StockQuantity INT
+);
+
+INSERT INTO #ProductsUpdate
+VALUES
+(1, 'USB Flash', 95, 200),
+(50, 'Wireless Mouse', 350, 100);
+
+
+MERGE Products AS Target
+USING #ProductsUpdate AS Source
+ON Target.ProductID = Source.ProductID
+
+WHEN MATCHED THEN
+    UPDATE SET 
+        Target.ProductName = Source.Name,
+        Target.UnitPrice = Source.UnitPrice,
+        Target.StockQuantity = Source.StockQuantity
+
+WHEN NOT MATCHED THEN
+    INSERT (ProductName, UnitPrice, StockQuantity, CategoryID)
+    VALUES (Source.Name, Source.UnitPrice, Source.StockQuantity, 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
